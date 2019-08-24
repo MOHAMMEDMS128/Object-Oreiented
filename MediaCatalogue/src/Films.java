f15779c756b4a9956625d8d943fa0d842f1f3189
Your Name HereMOHAMMEDMS128 <mohammed_ms128@hotmail.coM>
2019-08-24 11:46:53 +0100

    Initial commit

---
+++ MediaCatalogue/src/Films.java
@@ -0,0 +1,57 @@
+
+public class Films {
+	
+	// Attribute 
+	private String Title;
+	int YearOfRelease;
+	int Duration;
+	private String Studio;
+	private String Director;
+	int StarRating;
+	
+	// Methods
+	public String getTitle() {
+		return Title;
+	}
+	public void setTitle(String title) {
+		Title = title;
+	} // getTitle
+	public int getYearOfRelease() {
+		return YearOfRelease;
+	} // getYearOfRelease
+	public void setYearOfRelease(int yearOfRelease) {
+		YearOfRelease = yearOfRelease;
+	} // setYearOfRelease
+	public int getDuration() {
+		return Duration;
+	} // getDuration
+	public void setDuration(int duration) {
+		Duration = duration;
+	} // setDuration
+	
+	public String getStudio() {
+		return Studio;
+	} // getStudio
+	public void setStudio(String studio) {
+		Studio = studio;
+	} // setStudio
+	
+	public String getDirector() {
+		return Director;
+	} // getDirector
+	public void setDirector(String director) {
+		Director = director;
+	} // setDirector
+	
+	public int getStarRating() {
+		return StarRating;
+	} // getStarRating
+	public void setStarRating(int starRating) {
+		StarRating = starRating;
+	} // setStarRating
+	
+	
+	
+
+} // end class
+
